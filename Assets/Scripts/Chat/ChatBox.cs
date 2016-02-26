c3c0b7192d1a7b0d79d453fe0e3fd465febde3b2
Reck Hou <reckhou@gmail.com>
2016-02-26 20:28:46 +0800

    UI supports multiple resolution.

--- Assets/Scripts/Chat/ChatBox.cs
+++ Assets/Scripts/Chat/ChatBox.cs
@@ -4,161 +4,180 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class ChatBox : MonoBehaviour {
-	public Text TextBox;
-	public float TextSpeed;
-	private float textDeltaTime;
-	private List<string> textList;
-	private List<char> textBuffer;
-	private float lastUpdateTime;
-	private GameController gameController;
-	private Image avatar;
+public class ChatBox : MonoBehaviour
+{
+		public Text TextBox;
+		public float TextSpeed;
+		private float textDeltaTime;
+		private List<string> textList;
+		private List<char> textBuffer;
+		private float lastUpdateTime;
+		private GameController gameController;
+		private Image avatar;
 
-	public Sprite DoctorAvatar;
-	public Sprite SubjectAvatar;
-	public AudioSource pressAudio;
-	public AudioSource typeAudio;
-	public Button nextButton;
+		public Sprite DoctorAvatar;
+		public Sprite SubjectAvatar;
+		public AudioSource pressAudio;
+		public AudioSource typeAudio;
+		public Button nextButton;
 
-	public bool playTypeAudio;
+		public bool playTypeAudio;
 
-	private bool textPlaying;
-	public bool isDebug;
+		private bool textPlaying;
+		public bool isDebug;
 
-	public enum Character {
-		Doctor,
-		Subject
-	};
+		public enum Character
+		{
+				Doctor,
+				Subject}
 
-	// Use this for initialization
-	void Start () {
-		avatar = GameObject.Find("Avatar").GetComponent<Image>();
+		;
 
-		if (TextSpeed != 0) {
-			textDeltaTime = 1.0f/TextSpeed;
-		} else {
-			textDeltaTime = 1;
+		void Awake ()
+		{
+				textBuffer = new List<char> ();
+
 		}
 
-		textBuffer = new List<char>();
+		// Use this for initialization
+		void Start ()
+		{
+				avatar = GameObject.Find ("Avatar").GetComponent<Image> ();
 
-		gameController = GameObject.Find("GameController").GetComponent<GameController>();
-		if (gameController == null) {
-			print ("failed to get game controller!");
+				if (TextSpeed != 0) {
+						textDeltaTime = 1.0f / TextSpeed;
+				} else {
+						textDeltaTime = 1;
+				}
+						
+				gameController = GameObject.Find ("GameController").GetComponent<GameController> ();
+				if (gameController == null) {
+						print ("failed to get game controller!");
+				}
+
+				textList = new List<string> ();
+				playTypeAudio = true;
 		}
 
-		textList = new List<string>();
-		playTypeAudio = true;
-	}
-
-	public void SetAvatar(Character type) {
-		if (type == Character.Doctor) {
-			avatar.sprite = DoctorAvatar;
-		} else if (type == Character.Subject) {
-			avatar.sprite = SubjectAvatar;
-		}
-	}
-
-	public void SetText(List<string> list) {
-		playTypeAudio = true;
-		clearText();
-		textList = new List<string> ();
-		if (list == null) {
-			return;
+		public void SetAvatar (Character type)
+		{
+				if (type == Character.Doctor) {
+						avatar.sprite = DoctorAvatar;
+				} else if (type == Character.Subject) {
+						avatar.sprite = SubjectAvatar;
+				}
 		}
 
-		textList.AddRange(list);
-		for (int i = 0; i < textList.Count; i++) {
-			textList[i] += "\n";
-		}
-		PlayLine();
-	}
+		public void SetText (List<string> list)
+		{
+				playTypeAudio = true;
+				clearText ();
+				textList = new List<string> ();
+				if (list == null) {
+						return;
+				}
 
-	public void AddText(string text) {
-		textList.Add(text + "\n");
-	}
-
-	public void PlayLine() {
-		updateButtonStatus ();
-
-		if (textList.Count < 1) {
-			textEnd();
-			return;
+				textList.AddRange (list);
+				for (int i = 0; i < textList.Count; i++) {
+						textList [i] += "\n";
+				}
+				PlayLine ();
 		}
 
-		if (textList[0] == "\r\n") {
-			clearText();
-			textList.RemoveAt(0);
+		public void AddText (string text)
+		{
+				textList.Add (text + "\n");
 		}
 
-		if (textList.Count < 1) {
-			textEnd();
-			return;
+		public void PlayLine ()
+		{
+				updateButtonStatus ();
+
+				if (textList.Count < 1) {
+						textEnd ();
+						return;
+				}
+
+				if (textList [0] == "\r\n") {
+						clearText ();
+						textList.RemoveAt (0);
+				}
+
+				if (textList.Count < 1) {
+						textEnd ();
+						return;
+				}
+				List<char> tmpList = textList [0].ToList ();
+				textBuffer.AddRange (tmpList);
+				textBuffer.Add ('\n');
+				textList.RemoveAt (0);
+				if (!typeAudio.isPlaying && playTypeAudio) {
+						typeAudio.Play ();
+				}
 		}
-		List<char> tmpList = textList[0].ToList();
-		textBuffer.AddRange(tmpList);
-		textBuffer.Add('\n');
-		textList.RemoveAt(0);
-		if (!typeAudio.isPlaying && playTypeAudio) {
-			typeAudio.Play();
-    	}
-	}
 
-	void textEnd() {
-		gameController.TextEndCallback();
-	}
-
-	public void PlayPressAudio() {
-		pressAudio.Play();
-	}
-
-	public void Update() {
-		updateButtonStatus ();
-	}
-
-	void updateButtonStatus() {
-		if (textPlaying && !isDebug) {
-			nextButton.interactable = false;
-			return;		
-		} else {
-			nextButton.interactable = true;
+		void textEnd ()
+		{
+				gameController.TextEndCallback ();
 		}
-	}
 
-	public void CheckStatus() {
-		if (Time.time - lastUpdateTime > textDeltaTime) {
-			updateText();
-			lastUpdateTime = Time.time;
+		public void PlayPressAudio ()
+		{
+				pressAudio.Play ();
 		}
+
+		public void Update ()
+		{
+				updateButtonStatus ();
+		}
+
+		void updateButtonStatus ()
+		{
+				if (textPlaying && !isDebug) {
+						nextButton.interactable = false;
+						return;		
+				} else {
+						nextButton.interactable = true;
+				}
+		}
+
+		public void CheckStatus ()
+		{
+				if (Time.time - lastUpdateTime > textDeltaTime) {
+						updateText ();
+						lastUpdateTime = Time.time;
+				}
 		
 //		if (Input.GetKeyUp("space")) {
 //			PlayLine();
 //    	}
-	}
-
-	private void updateText() {
-		if (textBuffer == null) {
-			textBuffer = new List<char>();
 		}
 
-		if (textBuffer.Count < 1) {
-			textPlaying = false;
-			return;
+		private void updateText ()
+		{
+				if (textBuffer == null) {
+						textBuffer = new List<char> ();
+				}
+
+				if (textBuffer.Count < 1) {
+						textPlaying = false;
+						return;
+				}
+
+				textPlaying = true;
+				string curText = TextBox.text;
+				curText += textBuffer [0];
+				TextBox.text = curText;
+				textBuffer.RemoveAt (0);
+
+				if (!typeAudio.isPlaying && playTypeAudio) {
+						typeAudio.Play ();
+				}
 		}
 
-		textPlaying = true;
-		string curText = TextBox.text;
-		curText += textBuffer[0];
-		TextBox.text = curText;
-		textBuffer.RemoveAt(0);
-
-		if (!typeAudio.isPlaying && playTypeAudio) {
-			typeAudio.Play();
+		private void clearText ()
+		{
+				textBuffer.Clear ();
+				TextBox.text = "";
 		}
-	}
-
-	private void clearText() {
-		textBuffer.Clear();
-		TextBox.text = "";
-	}
 }
